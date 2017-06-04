f09b3dc5974cccfc3d0e0bcec8ac3cc372f21a9e
wildswallow <jlu33@calpoly.edu>
2017-06-03 22:25:26 -0700

    initial commit

---
+++ lab10.py
@@ -0,0 +1,14 @@
+# Name:         Jason Lu
+# Course:       CPE 101
+# Instructor:   Nupur Garg
+# Assignment:   Lab 10
+# Term:         Spring 2017
+
+import lab10helper
+
+def main():
+    print("Just a simple file.")
+    print(lab10helper.name())
+
+if __name__ == '__main__':
+    main()
